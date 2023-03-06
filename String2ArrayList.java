35df541b09ea884cddc85f9a9a9e8f296157bd93
SneekyStryder <106488424+SneekyStryder@users.noreply.github.com>
2023-03-06 12:03:20 -0500

    Create String2ArrayList.java

---
+++ String2ArrayList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class String2ArrayList
+{
+    private ArrayList<String> splitString;
+
+    public String2ArrayList()
+    {
+        splitString = new ArrayList<>();
+    }
+    public void splitAndAdd(String str)
+    {
+        for (int i = )
+    }
+    public String toString()
+    {
+        return "";
+    }
+}
